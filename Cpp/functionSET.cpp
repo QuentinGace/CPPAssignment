c79cf3a7a5aa01caf94d29db446cd8abb019e853
lucgmn <l.gomanne@net.estia.fr>
2020-11-10 19:25:38 +0000

    first draft of function class

--- Cpp/functionSET.cpp
+++ Cpp/functionSET.cpp
@@ -1,1 +1,44 @@
 #include "../Header/functionSET.h"
+#include <math.h>       //for exp()
+
+//***** TO DO : define all the methods *****
+
+double Function::get_fValue() const
+{
+	return(fValue);
+}
+
+double Function::norm1(double fValue)
+{
+
+}
+
+double Function::norm2(double fValue)
+{
+    
+}
+
+double Function::norm3(double fValue)
+{
+    
+}
+
+double Function::norm1(double fValue)
+{
+    
+}
+
+Set1::Set1(double x, double t)
+{
+
+}
+
+double Set1::sign(double x)
+{
+
+}
+
+Set2::Set2(double x, double t)
+{
+    fValue = exp(x)
+}
