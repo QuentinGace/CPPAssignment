844019e4875e558b2e85b3995bb7494cda862bcb
QuentinG50 <60266099+QuentinG50@users.noreply.github.com>
2020-11-11 09:54:49 +0000

    project ok

---
+++ CPPAssignement/CPPAssignement/functionSET.cpp
@@ -0,0 +1,40 @@
+#include "functionSET.h"
+#include <math.h>       //for exp()
+
+//***** TO DO : define all the methods *****
+
+double Function::get_fValue() const
+{
+	return(fValue);
+}
+
+double Function::norm1(double fValue)
+{
+    return 0;
+}
+
+double Function::norm2(double fValue)
+{
+    return 0;
+}
+
+double Function::norm3(double fValue)
+{
+    return 0;
+}
+
+
+Set1::Set1(double x, double t)
+{
+
+}
+
+double Set1::sign(double x)
+{
+    return 0;
+}
+
+Set2::Set2(double x, double t)
+{
+    fValue = exp(x);
+}
---
+++ CPPAssignement/CPPAssignement/main.cpp
@@ -0,0 +1,158 @@
+// Assignment CPP Gr3
+// Quentin Parmeshwari Luc
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f0(double x);
+double f1(double x);
+
+double f0t(double x, double t);
+double f1t(double x, double t);
+
+double upwind(double f00, float f01); 			// f00 = f(n,i-1)  f01 = f(n,i)
+double central(double f00, float f01, float f02); 	// f00 = f(n,i-1)  f01 = f(n,i)   f02 = f(n,i+1)
+
+int u = 1;
+double dx = 0.8;
+double dt = 0.5;
+double v = u * dt / dx;
+
+
+int main()
+{
+	double tab0[100][100]; 				// which stock f0 values
+	double tab1[100][100];				// which stock f1 values
+
+// 	INITIAL CONDITIONS at t=0
+
+	tab0[0][0] = 0;
+	for (int i = 1; i < 99; i++)
+	{
+		tab0[0][i] = f0(-40 + i * dx);
+	}
+	tab0[0][99] = 1;
+
+	tab1[0][0] = 0;
+	for (int i = 1; i < 99; i++)
+	{
+		tab1[0][i] = f1(-40 + i * dx);
+	}
+	tab1[0][99] = 1;
+
+
+	// 	x from [-40,40] with a step of dx = 0.8
+	// 	choose either upwind or central method 
+
+	for (int n = 1; n < 100; n++)
+	{
+		tab0[n][0] = 0;
+		for (int i = 1; i < 99; i++)
+		{
+			tab0[n][i] = upwind(tab0[n - 1][i - 1], tab0[n - 1][i]);
+			//tab0[n][i] = central(tab0[n-1][i-1],tab0[n-1][i],tab0[n-1][i+1]);
+		}
+		tab0[n][99] = 1;
+	}
+
+	for (int n = 1; n < 100; n++)
+	{
+		tab1[n][0] = 0;
+		for (int i = 1; i < 99; i++)
+		{
+			//tab1[n][i] = upwind(tab1[n-1][i-1],tab1[n-1][i]);
+			tab1[n][i] = central(tab1[n - 1][i - 1], tab1[n - 1][i], tab1[n - 1][i + 1]);
+		}
+		tab1[n][99] = 1;
+	}
+
+	double tab0an[5][3];
+	double tab1an[5][3];
+
+
+	tab0an[0][0], tab1an[0][0] = f0t(-40, 5), f1t(-40, 5);
+	tab0an[1][0], tab1an[1][0] = f0t(-20, 5), f1t(-20, 5);
+	tab0an[2][0], tab1an[2][0] = f0t(0, 5), f1t(0, 5);
+	tab0an[3][0], tab1an[3][0] = f0t(20, 5), f1t(20, 5);
+	tab0an[4][0], tab1an[4][0] = f0t(40, 5), f1t(40, 5);
+
+	tab0an[0][1], tab1an[0][1] = f0t(-40, 10), f1t(-40, 10);
+	tab0an[1][1], tab1an[1][1] = f0t(-20, 10), f1t(-20, 10);
+	tab0an[2][1], tab1an[2][1] = f0t(0, 10), f1t(0, 10);
+	tab0an[3][1], tab1an[3][1] = f0t(20, 10), f1t(20, 10);
+	tab0an[4][1], tab1an[4][1] = f0t(40, 10), f1t(40, 10);
+
+	tab0an[0][2], tab1an[0][2] = f0t(-40, 20), f1t(-40, 20);
+	tab0an[1][2], tab1an[1][2] = f0t(-20, 20), f1t(-20, 20);
+	tab0an[2][2], tab1an[2][2] = f0t(0, 20), f1t(0, 20);
+	tab0an[3][2], tab1an[3][2] = f0t(20, 20), f1t(20, 20);
+	tab0an[4][2], tab1an[4][2] = f0t(40, 20), f1t(40, 20);
+
+	// Print the values of both tables
+
+	cout << "t = 5" << endl;
+	for (int i = 0; i < 100; i++)
+	{
+		cout << "f0(" << -40 + (i + 1) * dx << ",5) = " << tab0[10][i] << "	:: f1(" << -40 + (i + 1) * dx << ",5) = " << tab1[10][i] << endl;
+	}
+
+	cout << "t = 10" << endl;
+	for (int i = 0; i < 100; i++)
+	{
+		cout << "f0(" << -40 + (i + 1) * dx << ",10) = " << tab0[20][i] << "	:: f1(" << -40 + (i + 1) * dx << ",10) = " << tab1[20][i] << endl;
+	}
+
+	cout << "t = 20" << endl;
+	for (int i = 0; i < 100; i++)
+	{
+		cout << "f0(" << -40 + (i + 1) * dx << ",20) = " << tab0[40][i] << "	:: f1(" << -40 + (i + 1) * dx << ",20) = " << tab1[40][i] << endl;
+	}
+
+	cout << "analytical solutions" << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << "f0.an(" << -40 + (20 * i) << "," << (j + 1) * 5 << ") = " << tab0an[i][j] << "	:: f1.an(" << -40 + (20 * i) << "," << (j + 1) * 5 << ") = " << tab1an[i][j] << endl;
+		}
+	}
+	return 0;
+
+}
+
+
+double f0(double x)
+{
+	if (x < 0) return 0;
+	if (x == 0) return 0.5;
+	else return 1;
+}
+
+double f1(double x)
+{
+	return 0.5 * exp(-1 * (x * x));
+}
+
+double upwind(double f00, float f01)
+{
+	return v * f00 + (1 - v) * f01;			// f(n+1,i) = v*f(n,i-1) + (1-v)*f(n,i)
+}
+
+double central(double f00, float f01, float f02)
+{
+	return (-v * f02) / 2 + f01 + v * f00 / 2;		// f(n+1,i) = -v*f(n,i+1)/2 + f(n,i) + v*f(n,i-1)/2
+}
+
+double f0t(double x, double t)
+{
+	if ((x - t) < 0) return 0;
+	if ((x - t) == 0) return 0.5;
+	else return 1;
+}
+
+double f1t(double x, double t)
+{
+	return 0.5 * exp(-1 * (x - t) * (x - t));
+}
