2f688545c2674e5d9993a6cb9160e502f816586d
lucgmn <l.gomanne@net.estia.fr>
2020-11-11 00:52:28 +0000

    second draft for function class

--- Cpp/functionSET.cpp
+++ Cpp/functionSET.cpp
@@ -1,44 +1,29 @@
 #include "../Header/functionSET.h"
 #include <math.h>       //for exp()
 
-//***** TO DO : define all the methods *****
 
-double Function::get_fValue() const
+
+Function::Function() : f(0), af(0) {}
+
+Function::Function(func f) : f(f) {}
+
+Function::Function(Afunc f) : af(f) {}
+
+Function::Function(func f1, Afunc f2) : f(f1), af(f2) {}
+
+Function& Function::setFunc(func f)
 {
-	return(fValue);
+    f = f;
+    return *this;
 }
 
-double Function::norm1(double fValue)
+Function& Function::setAfunc(Afunc f)
 {
-
+    af = f;
+    return *this;
 }
 
-double Function::norm2(double fValue)
+void Function::display() const
 {
-    
-}
-
-double Function::norm3(double fValue)
-{
-    
-}
-
-double Function::norm1(double fValue)
-{
-    
-}
-
-Set1::Set1(double x, double t)
-{
-
-}
-
-double Set1::sign(double x)
-{
-
-}
-
-Set2::Set2(double x, double t)
-{
-    fValue = exp(x)
+    std::cout << "numerical : " << f << "analitical :" << af << std::endl;
 }
--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,5 +1,40 @@
 // Assignment CPP Gr3
 //Quentin Parmeshwari Luc
+#include "../Header/functionSET.h"
 
-//hello guys
-//hello world
+
+int u = 1;
+double dx = 0.8;
+double dt = 0.5;
+double v = u*dt/dx;
+
+double Set1(double x);
+double ASet1(double x, double t);
+double sign(double x);
+
+
+int main()
+{
+    Function f1(Set1);
+    Function Af1(ASet1);
+    f1.display();
+    return 0;
+}
+
+
+double Set1(double x)
+{
+    return (sign(x)+1)/2;
+}
+
+double ASet1(double x, double t)
+{
+    return (sign(x-1.75*t)+1)/2;
+}
+
+double sign(double x)
+{
+	if (x<0) return 0;
+	if (x==0) return 0.5;
+	else return 1;
+}
